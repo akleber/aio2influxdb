f2b31f6ff167b271d85b95ae69edf5212e7e2dc9
Andreas Kleber <andreas@drosselweg7a.de>
2019-06-30 08:13:04 +0200

    refactoring, test mode

--- aio2influxdb.py
+++ aio2influxdb.py
@@ -11,6 +11,7 @@ import sys
 DB_NAME = 'aio-test'
 AIO_HOST = '192.168.178.23'
 FREQUENCY = 5
+TEST_MODE = False
 
 
 def write_data(db, time, measurement, value):
@@ -23,57 +24,38 @@ def write_data(db, time, measurement, value):
             }
         }
     ]
-    db.write_points(json_body)
+    if db:
+        db.write_points(json_body)
 
 
-def fetch_and_write(db):
+def fetch_html():
+    content = ""
+
+    if not TEST_MODE:
+        timeout = math.floor(FREQUENCY * 0.6)
+        r = requests.get("http://{}:21710/F0".format(AIO_HOST), timeout=timeout)
+        if r.ok:
+            content = r.text
+
+    return content
+
+
+def parse_and_write(db, html):
     time = datetime.datetime.utcnow()
-    timeout = math.floor(FREQUENCY * 0.6)
 
-    r = requests.get("http://{}:21710/F0".format(AIO_HOST), timeout=timeout)
-    content = r.text
-    if not r.ok:
+    if not html:
         return
 
-    grid_p = 0
-    load_p = 0
-    pv_p = 0
-    inv_p = 0
-    bt_p = 0
-    bt_soc = 0
+    measurements = ['GRID_P', 'LOAD_P', 'PV_P', 'INV_P', 'BT_P', 'BT_SOC', 'Temp']
 
-    m = re.search(r"""GRID_P</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        grid_p = m.group(1)
+    for measurement in measurements:
+        regexp = measurement + r"""</td><td>(-?\d+\.+\d+)</td>"""
+        m = re.search(regexp, html)
+        if m:
+            value = m.group(1)
 
-    m = re.search(r"""LOAD_P</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        load_p = m.group(1)
-
-    m = re.search(r"""PV_P</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        pv_p = m.group(1)
-
-    m = re.search(r"""INV_P</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        inv_p = m.group(1)
-
-    m = re.search(r"""BT_P</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        bt_p = m.group(1)
-
-    m = re.search(r"""BT_SOC</td><td>(-?\d+\.+\d+)</td>""", content)
-    if m:
-        bt_soc = m.group(1)
-
-    write_data(db, time, "grid_p", grid_p)
-    write_data(db, time, "load_p", load_p)
-    write_data(db, time, "pv_p", pv_p)
-    write_data(db, time, "inv_p", inv_p)
-    write_data(db, time, "bt_p", bt_p)
-    write_data(db, time, "bt_soc", bt_soc)
-
-    logging.info('Data written: {}; {}; {}; {}; {}; {}'.format(grid_p, load_p, pv_p, inv_p, bt_p, bt_soc))
+            write_data(db, time, measurement, value)
+            logging.info('Data written: {}: {}'.format(measurement, value))
 
 
 def main():
@@ -85,15 +67,19 @@ def main():
         ])
     logging.info('Startup')
 
-    dbclient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database=DB_NAME)
-    logging.info('Database connection established')
+    dbclient = None
+    if not TEST_MODE:
+        dbclient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database=DB_NAME)
+        logging.info('Database connection established')
 
-    dbclient.create_database(DB_NAME)
-    logging.info("Ensure database: " + DB_NAME)
+        dbclient.create_database(DB_NAME)
+        logging.info("Ensure database: " + DB_NAME)
 
     while True:
         try:
-            fetch_and_write(dbclient)
+            html = fetch_html()
+            parse_and_write(dbclient, html)
+
             time.sleep(FREQUENCY)
         except Exception:
             logging.exception("Exception")
