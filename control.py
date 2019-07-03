43f8db2634ade02fd033fceac938cff0087dbceb
Andreas Kleber <andreas@drosselweg7a.de>
2019-07-03 22:23:07 +0200

    test for pip

--- control.py
+++ control.py
@@ -60,9 +60,11 @@ def rebase():
 
 @app.route('/pip')
 def pip():
-    command = ['python', '_venv/bin/pip', 'install', '-r', 'requirements.txt']
-    result = run(command, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    return render_template('control.html', code=result.stdout+result.stderr)
+    #command = ['python', '_venv/bin/pip', 'install', '-r', 'requirements.txt']
+    command = ['pip', '--help']
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    code = result.stdout + result.stderr
+    return render_template('control.html', code=code.strip())
 
 
 @app.route('/restart')
